833934ec914683d09ac06dd0f636a6735482eb55
Jose Córdoba <josew92992@gmail.com>
2025-08-18 23:16:40 -0500

    Parcialmente completada Parte 5: añadido bucle principal, pendiente corregir lógica de jugar_de_nuev

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,13 +1,8 @@
 import random
 p_p_t = ["Piedra", "Papel", "Tijera"]
+puntaje_user = 0
+puntaje_compu = 0
 
-
-vict_user = 0
-vict_compu = 0
-
-def puntaje():
-    print(f"Tú llevas ,{vict_user}, puntos")
-    print(f"La computadora lleva ,{vict_compu}, puntos")
 def compu_w():
     print("La computadora gana!")
 def user_w():
@@ -15,61 +10,40 @@ def user_w():
 def tie():
     print("Ha habido un empate parcial")
 
-def jugar(compu,user):
 
-    user = input("Ingresa \"Piedra\", \"Papel\" o \"Tijera\": ")
-    user = user.title()
+
+def jugar():
+    #Entrada del usuario y elección de la computadora
+    elec_usuario = input("Ingresa \"Piedra\", \"Papel\" o \"Tijera\": ")
+    elec_usuario = elec_usuario.title()
     compu = random.choice(p_p_t)
+    #Usar puntajes globales
+    global puntaje_user
+    global puntaje_compu
 
-    try:
-        while user not in p_p_t:
-            user = input("Ingresa \"Piedra\", \"Papel\" o \"Tijera\": ")
-            user = user.title()
-            if user.title() not in ppt:
-                user = input("Ingresa \"Piedra\", \"Papel\" o \"Tijera\": ")
-            user = user.title()
-            print( "Entrada no válida, elige Piedra, Papel o Tijera")
-            
-            
+    #Validar entrada del ususario
+    while elec_usuario not in p_p_t:
+        elec_usuario = input("Ingresa \"Piedra\", \"Papel\" o \"Tijera\": ")
+        elec_usuario = elec_usuario.title()
+        print( "Entrada no válida, elige Piedra, Papel o Tijera")
+        if elec_usuario in p_p_t:
+            break
+    #Imprimir elección de usuario y computadora
+    print(f"La computadora elegió: {compu}")
+    print(f"Tú elejiste: {elec_usuario}")
+    #Compara las elecciones y da el veredicto
+    if compu == "Tijera" and elec_usuario == "Piedra" or compu == "Piedra" and elec_usuario == "Papel" or compu == "Papel" and elec_usuario == "Tijera":
+        user_w()
+        puntaje_user +=1
+    if compu == "Tijera" and elec_usuario == "Papel" or compu == "Piedra" and elec_usuario == "Tijera" or compu == "Papel" and elec_usuario == "Piedra":
+        compu_w()
+        puntaje_compu +=1
+    if compu == elec_usuario:
+        tie()
+    print (f"Puntaje:\nTú = {puntaje_user} \nComputadora = {puntaje_compu}")
 
-
-
-        if compu == "Tijera" and user == "Pierda":
-            user_w()
-        if compu == "Tijera" and user == "Papel":
-            compu_w()
-        if compu == "Piedra" and user == "Tijera":
-            compu_w()
-        if compu == "Piedra" and user == "Papel":
-            user_w()
-        if compu == "Papel" and user == "Tijera":
-            user_w()
-        if compu == "Papel" and user == "Piedra":
-            compu_w()
-        if compu == "Piedra" and user == "Piedra":
-            tie()
-        if compu == "Tijera" and user == "Tijera":
-            tie()
-        if compu == "Papel" and user == "Papel":
-            tie()
-            return
-    except TypeError:
-        return "Error"
-
-print(compu)
-print(user)
-
-
-
-
-jugar(compu, user)
-
-if jugar(compu_w,user_w):
-    vict_compu +=1
-if jugar(user_w,user_w):
-    vict_user +=1
-else:
-    print("")
-
-print("")
-print("")
+while True:#Bucle principal para repetir rondas del juego
+    jugar()
+    jugar_de_nuevo = input("Jugar otra ronda?: ")
+    jugar_den_nuevo = jugar_de_nuevo.lower()
+    
